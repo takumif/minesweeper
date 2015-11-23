7d4033adb6230b9142191369acf2ecd63802e628
Takumi <takumi@takumium.com>
2015-11-23 18:00:17 -0500

    TextMinesweeper is playable except for win condition

--- TextMinesweeper.ts
+++ TextMinesweeper.ts
@@ -9,76 +9,106 @@ import RectMinesweeper = require('./RectMinesweeper');
  * Supports one board size up to 36x36
  */
 class TextMinesweeper implements MSObserver {
-	private indices = '0123456789abcdefghijklmnopqrstuvwxyz';
-	private ms: Minesweeper;
-	private rows: number;
-	private cols: number;
-	private bombCount: number;
-	
-	constructor() {
-		this.rows = 8;
-		this.cols = 8;
-		this.bombCount = 8;
-		
-		console.log('gonna instantiate Rect');
-		this.ms = new RectMinesweeper(this.rows, this.cols, this.bombCount);
-		this.ms.addObserver(this);
-		console.log("gonna play");
-		this.ms.play();
-	}
-	
-	onGameStart() {
-		this.printBoard();
-	}
-	
-	onWaitingInput() {
-		input('Please enter a move (e.g. "1,2" for row 1, col 2): ', (move: string) => {
-			console.log('Move is:' + move);
-		});
-	}
-	
-	onFieldChanged() {
-		
-	}
-	
-	onBombStepped(row: number, col: number) {
-		
-	}
-	
-	onVictory() {
+	private indices = '123456789abcdefghijklmnopqrstuvwxyz';
+    private ms: Minesweeper;
+    private rows: number;
+    private cols: number;
+    private bombCount: number;
+
+    constructor() {
+        this.rows = 8;
+        this.cols = 8;
+        this.bombCount = 10;
+
+        this.ms = new RectMinesweeper(this.rows, this.cols, this.bombCount);
+        this.ms.addObserver(this);
+        this.ms.play();
+    }
+
+    onGameStart() {
+        this.printBoard();
+    }
+
+    onWaitingInput() {
+        this.getUserInput();
+    }
+
+    onFieldChanged() {
+        this.printBoard();
+    }
+
+    onBombStepped(row: number, col: number) {
+        console.log('Stepped on a bomb!');
+    }
+
+    onVictory() {
 		
 	}
-	
-	private printBoard(): void {
-		var firstRowStr = '  ' + this.indices.split('').join(' ');
-		console.log(firstRowStr);
-		console.log(); // a blank row
-		
-		for (var row = 0; row < this.rows; row++) {
-			var rowArr = [this.indices[row]];
-			
-			for (var col = 0; col < this.cols; col++) {
-				var cell = this.ms.getCellAt(row, col);
-				rowArr.push(this.getCellSymbol(cell));
-			}
-			
-			console.log(rowArr.join(' '));
-		}
+
+    private getUserInput(): void {
+        input('Please enter a move (e.g. "1,2" for row 1, col 2): ', (moveStr: string) => {
+            var move: { row: number, col: number } = this.parseMove(moveStr);
+
+            if (this.isValidMove(move)) {
+                this.ms.makeMove(move.row, move.col);
+            } else {
+                console.log('Invalid move!');
+                this.getUserInput();
+            }
+        });
+    }
+
+    private parseMove(move: string): { row: number, col: number } {
+        var rowColStr = move.split(',');
+
+        if (rowColStr.length < 2) {
+            return {row: NaN, col: NaN};
+        }
+
+        return {row: this.indices.indexOf(rowColStr[0]),
+            col: this.indices.indexOf(rowColStr[1])};
 	}
-	
-	private getCellSymbol(cell: Cell): string {
-		if (cell.open) {
-			if (cell.bomb) {
-				return 'X';
-			} else if (cell.adjBombCount > 0) {
-				return cell.adjBombCount.toString();
-			} else {
-				return ' ';
-			}
-		}
+
+    private isValidMove(move: { row: number, col: number }): boolean {
+        if (move.row === NaN || move.col === NaN) {
+            return false;
+        }
+
+        return this.ms.isValidMove(move.row, move.col);
+    }
+
+    private printBoard(): void {
+        console.log();
+
+        var firstRowStr = '    ' + this.indices.slice(0, this.cols).split('').join(' ');
+        console.log(firstRowStr);
+        console.log(); // a blank row
 		
-		return 'O';
-	}
+        for (var row = 0; row < this.rows; row++) {
+            var rowArr = [this.indices[row], ' '];
+
+            for (var col = 0; col < this.cols; col++) {
+                var cell = this.ms.getCellAt(row, col);
+                rowArr.push(this.getCellSymbol(cell));
+            }
+
+            console.log(rowArr.join(' '));
+        }
+        console.log();
+    }
+
+    private getCellSymbol(cell: Cell): string {
+        if (cell.open) {
+            if (cell.bomb) {
+                return 'X';
+            } else if (cell.adjBombCount > 0) {
+                return cell.adjBombCount.toString();
+            } else {
+                return ' ';
+            }
+        }
+        return 'O';
+    }
 }
 
 new TextMinesweeper();
